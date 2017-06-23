2b671f5d4574b556397527c1844c4ebdcbf0963f
Enoch2k2 <enoch2k2@hotmail.com>
2017-06-23 11:27:44 -0500

    updated code to also have a for loop

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main(){
     cout << "i's value in the while loop is : " << i + 1 << endl;
     i++;
   }
-  
+  int count = 5;
+  for (size_t i = 0; i < count; i++) {
+    cout << "i's value in the for loop is :" << i + 1 << endl;
+  }
   return 0;
 }
