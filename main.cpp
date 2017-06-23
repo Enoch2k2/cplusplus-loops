fba38bc888242d060dac519e5509bdc99b1b32a9
Enoch2k2 <enoch2k2@hotmail.com>
2017-06-23 10:54:36 -0500

    created a while loop

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int i = 0;
+  while(i < 5){
+    cout << "i's value in the while loop is : " << i + 1 << endl;
+    i++;
+  }
+  
+  return 0;
+}
